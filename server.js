723252b124d83d62db1c18f4a85fb98a594b5741
Mountainmancodes <Colorado.JHernandez@gmail.com>
2024-08-19 00:14:43 -0600

    fix pathing issue

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 require('dotenv').config();
+const path = require('path'); // Make sure this line is at the top
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const sequelize = require('./config/connection');
 const routes = require('./controllers');
+const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+if (!process.env.SESSION_SECRET) {
+  console.error('SESSION_SECRET is not defined in the environment variables.');
+  process.exit(1); // Exit the process to prevent running without a session secret
+}
+
 const sess = {
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET || 'defaultSecret',
   cookie: {
     maxAge: 60 * 60 * 1000, // 1 hour
     httpOnly: true,
@@ -39,5 +45,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, () =>
+    console.log(
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`,
+    ),
+  );
 });
