9a3b6bb7d20c95a7b20f1da90a14a31e2c5a5209
Mountainmancodes <Colorado.JHernandez@gmail.com>
2024-08-18 19:43:37 -0600

    updated .env and restored connections.js

--- config/connections.js
+++ config/connections.js
@@ -1,14 +1,16 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'postgres', // Ensure this matches your database type
-  }
-);
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'postgres', // Ensure this matches your database type
+      }
+    );
 
 module.exports = sequelize;
