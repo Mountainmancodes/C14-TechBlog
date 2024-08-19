4d86d5e171be89ef58b5f0182b60739eff1e2b18
Mountainmancodes <Colorado.JHernandez@gmail.com>
2024-08-18 23:54:55 -0600

    attempt to address error in Render related to undfined user

--- models/User.js
+++ models/User.js
@@ -1,3 +1,13 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
 User.init(
   {
     id: {
@@ -9,21 +19,28 @@ User.init(
     username: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true, // Ensure that usernames are unique
+      unique: true,
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [8], // Ensure the password is at least 8 characters long
+        len: [8],
       },
     },
   },
   {
     hooks: {
-      async beforeCreate(newUserData) {
+      beforeCreate: async (newUserData) => {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10,
+        );
+        return updatedUserData;
       },
     },
     sequelize,
@@ -31,5 +48,7 @@ User.init(
     freezeTableName: true,
     underscored: true,
     modelName: 'user',
-  }
+  },
 );
+
+module.exports = User;
